09e2aade6e11d054d729df1486de13d9c435ae2f
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-27 16:36:03 -0300

    Aula_03
    - Criando um Banco de Dados
    - Escolhendo um Banco de Dados
    -  Verificando a Estrutura do Banco de Dados Criado

---
+++ Aula_03.sql
@@ -0,0 +1,14 @@
+/* Criando um Banco de Dados */
+CREATE DATABASE 
+	db_Biblioteca
+GO
+
+/* Escolhendo um Banco de Dados */
+USE
+	db_Biblioteca
+GO
+
+/* Verificando a Estrutura do Banco de Dados Criado */
+SP_HELPDB
+	db_biblioteca
+GO
