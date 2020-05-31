35667eb5c5bccde20c2ed96832553b3fe746a6c0
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-31 11:53:18 -0300

    Aula_29
    - Escolhendo uma Banco de Dados.
    - Backup do Banco de Dados.

---
+++ Aula_29.sql
@@ -0,0 +1,10 @@
+/* Escolhendo uma Banco de Dados */
+USE
+	db_Biblioteca
+GO
+
+/* Backup do Banco de Dados */
+BACKUP DATABASE
+	db_Biblioteca
+TO DISK = 'C:\Users\romul\OneDrive\Documentos\SQL Server Management Studio\Boson Treinamentos\Testando.bak';
+GO
